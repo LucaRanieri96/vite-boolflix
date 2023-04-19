a7a123a12e27d9f08fc9f50a5f21e2dd5b224f31
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-04-19 11:34:10 +0200

    Init Boolfix

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { store } from "./store.js";
+</script>
+
+<template>
+    
+</template>
+
+<style lang="scss">
+@use "/node_modules/bootstrap/scss/bootstrap.scss";
+@use "./styles/style.scss";
+</style>
---
+++ src/store.js
@@ -0,0 +1,21 @@
+import { reactive } from "vue";
+import axios from "axios";
+
+export const store = reactive({
+  apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=6148d52a02fe2b51e6f0b4078dcb1be4&query=Luca&page=1",
+  
+
+  fetchCards(url) {
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(response);
+        
+      })
+      .catch((error) => {
+        console.log(error);
+        console.error(error.message);
+      });
+  },
+  
+});
