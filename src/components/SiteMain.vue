50865c51339299fdf7c1f2a837619620d325e4b3
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-04-19 18:10:28 +0200

    add method "changeScore"

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -8,6 +8,11 @@ export default {
       store,
     };
   },
+  methods: {
+    changeScore(score) {
+      return Math.round(Number(score) / 2);
+    }
+  }
   
 };
 </script>
@@ -20,7 +25,7 @@ export default {
         <h3>Title: {{ movie.title }}</h3>
         <p>Original title: {{ movie.original_title }}</p>
         <div v-html="store.langToFlag(movie.original_language)"></div>
-        <p>Score: {{ movie.vote_average }}</p>
+        <p>Score: {{ changeScore(movie.vote_average) }}</p>
       </li>
     </ul>
     <ul>
@@ -30,7 +35,7 @@ export default {
         <h3>Title: {{ tvshow.name }}</h3>
         <p>Original title: {{ tvshow.original_title }}</p>
         <div v-html="store.langToFlag(movie.original_language)"></div>
-        <p>Score: {{ tvshow.vote_average }}</p>
+        <p>Score: {{ changeScore(tvShow.vote_average) }}</p>
       </li>
     </ul>
 </template>
