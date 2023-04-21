cb64e8cac034f13efebe78607012b1ee9c6c2f5e
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-04-21 14:27:46 +0200

    better style card overview

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -23,6 +23,36 @@ export default {
 <template>
   <main>
     <div class="container-fluid p-5">
+      <!-- Film -->
+      <h2>Film</h2>
+      <ul class="row flex-row">
+        <li class="col-2 my-2 d-flex" v-for="movie in store.movies">
+          <div class="card text-bg-dark">
+            <img
+              class="card-img"
+              :src="movie.poster_path ? `${store.coverUrl}${movie.poster_path}`: 'src/assets/imgs/placeholder_image.jpg'
+              "
+            />
+            <div class="card-img-overlay">
+              <h3>{{ movie.title }}</h3>
+              <p>{{ movie.release_date }}</p>
+              <p class="card-text mb-4">
+              <strong>Original title:</strong> "{{ movie.original_title }}"
+              </p>
+              <p class="card-text"><strong>Description:</strong> {{ movie.overview }}</p>
+              <div class="my-2">
+                <div v-html="store.langToFlag(movie.original_language)"></div>
+              </div>
+              <div class="pb-3">
+                <p><strong>Score(1to10):</strong> {{ movie.vote_average }}</p>
+                <span
+                  v-for="star in starScore(changeScore(movie.vote_average))"
+                  :key="star">⭐️</span>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
       <!-- TVSHOW -->
       <h2>Tv Show</h2>
       <ul class="row flex-row">
@@ -34,46 +64,19 @@ export default {
               "
             />
             <div class="card-img-overlay">
-              <h4>{{ series.name }}</h4>
+              <h3>{{ series.name }}</h3>
+              <p>{{ series.first_air_date }}</p>
               <p class="card-text mb-4">
-                Original title: {{ series.original_name }}
+                <strong>Original title:</strong> {{ series.original_name }}
               </p>
-              <p class="card-text">{{ series.overview }}</p>
-              <div>
+              <p class="card-text"><strong>Description:</strong> {{ series.overview }}</p>
+              <div class="my-2">
                 <div v-html="store.langToFlag(series.original_language)"></div>
               </div>
-              <div>
-                <p>Score: {{ changeScore(series.vote_average) }}</p>
+              <div class="pb-1">
+                <p><strong>Score(1to10):</strong> {{ series.vote_average }}</p>
                 <span
                   v-for="star in starScore(changeScore(series.vote_average))"
-                  :key="star">⭐️</span>
-              </div>
-            </div>
-          </div>
-        </li>
-      </ul>
-      <h2>Film</h2>
-      <ul class="row flex-row">
-        <li class="col-2 my-2 d-flex" v-for="movie in store.movies">
-          <div class="card text-bg-dark">
-            <img
-              class="card-img"
-              :src="movie.poster_path ? `${store.coverUrl}${movie.poster_path}`: 'src/assets/imgs/placeholder_image.jpg'
-              "
-            />
-            <div class="card-img-overlay">
-              <h4>{{ movie.title }}</h4>
-              <p class="card-text mb-4">
-                Original title: {{ movie.original_title }}
-              </p>
-              <p class="card-text">{{ movie.overview }}</p>
-              <div>
-                <div v-html="store.langToFlag(movie.original_language)"></div>
-              </div>
-              <div>
-                <p>Score: {{ changeScore(movie.vote_average) }}</p>
-                <span
-                  v-for="star in starScore(changeScore(movie.vote_average))"
                   :key="star">⭐️</span>
               </div>
             </div>
